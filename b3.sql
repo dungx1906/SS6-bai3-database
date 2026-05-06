89130bb122c09b63ef500e5ff5fdb4b3afc7b6aa
dungx1906 <conanshinan@gmail.com>
2026-05-06 22:07:34 +0700

    no1

---
+++ b3.sql
@@ -0,0 +1,12 @@
+create database ss6;
+use ss6;
+
+SELECT s.Email
+FROM Students s
+WHERE NOT EXISTS (
+    SELECT 1 
+    FROM Payments p 
+    WHERE p.student_id = s.student_id 
+      AND p.payment_date >= '2024-01-01' 
+      AND p.payment_date < '2025-01-01'
+);
